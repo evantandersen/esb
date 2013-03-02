8ac199ecdd9ff16927ef4c51eec401b82af52ec9
Evan Andersen <evantandersen@overclocked.net>
2013-03-02 10:04:44 -0600

    Changed config file to match M3

--- master/esb_master.py
+++ master/esb_master.py
@@ -175,7 +175,7 @@ s.close()
 
 #create a config file
 config = open("esb-test.config", "w")
-config.write("server_host 0.0.0.0\nserver_port 3940\nusername admin\npassword xxxnq.BMCifhU\ntable test\n")
+config.write("server_host 0.0.0.0\nserver_port 3940\nusername admin\npassword xxxnq.BMCifhU\ntable test\ndata_directory .\nstorage_policy in-memory")
 config.close()
 
 #start the server
